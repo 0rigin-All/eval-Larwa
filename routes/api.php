61a106d9c82cd289ea503383643e3856be77a8ad
Origin-All <fabian.larwa.fl@gmail.com>
2023-04-07 15:20:22 +0200

    fonctionnalité api (sauf search)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Recette;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('/recettes', function(Request $request){
+    $recettes = Recette::select("id","titre","ingredients", "photo", "duree")->get();
+    return response()->json($recettes);
+});
+Route::delete('/recettes/{id}', function ($id) {
+    $recette = Recette::find($id);
+    $ok=$recette->delete();
+    if ($ok) {
+        return response()->json(["status" => 1, "message" => "recette supprimé"],200);
+        } else {
+        return response()->json(["status" => 0, "message" => "cette recette n'existe pas"],400);
+        }
+});
+Route::put('/recettes',function(Request $request){
+    $id = $request->id;
+    $recette = Recette::find($id);
+    $recette->titre = request('titre');
+    $recette->ingredients = request('ingredients');
+    $recette->duree = request('duree');
+    $recette->photo = request('photo');
+    $ok=$recette->save();
+    if ($ok) {
+        return response()->json(["status" => 1, "message" => "recette modifié"],200);
+        } else {
+        return response()->json(["status" => 0, "message" => "cette recette n'existe pas"],400);
+        }
 });
